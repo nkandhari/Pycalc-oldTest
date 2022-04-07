e1055281fd761574b4e397f3475102fc0279926f
Nitika Kandhari <nitika.kandhari@monash.edu>
2022-04-07 10:59:27 +1000

    edited pythonn script- challenge 1

--- pycalc.py
+++ pycalc.py
@@ -1,11 +1,15 @@
-# create function to add two numbers:
-def compute(expression):
+# The script takes in expression in the form: `a + b` and returns sum of a and b.
+# To run the script:
+# In the terminal, enter python IDE:
+# $ python3
+# $ from pycalc import compute  --> from the pycalc.py script, import compute function
+def compute(expression):# function name: compute
     values = expression.split(' ')
     num0 = int(values[0])
     operator = values[1]
-    num1 = int(value[2])
+    num1 = int(values[2])
     if operator == '+':
         return num0 + num1
     else:
         print('unknown operator!')
-        return 0
+        return None
