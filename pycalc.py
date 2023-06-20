c64b1a5e763dc5a1820c05418311503f05e0acf1
Nitika Kandhari <nitika.kandhari@monash.edu>
2023-06-20 11:18:30 +1000

    pycal add

--- pycalc.py
+++ pycalc.py
@@ -3,18 +3,15 @@
 # In the terminal, enter python IDE:
 # $ python3
 # $ from pycalc import compute  --> from the pycalc.py script, import compute function
-def compute(expression):# function name: compute
-    num0, operator, num1 = expression.split(' ')
+# example:
+# $ compute('2 + 2')
+def compute(expression):
+    values = expression.split(' ')
+    num0 = int(values[0])
+    operator = values[1]
+    num1 = int(values[2])
     if operator == '+':
         return num0 + num1
-    elif operator == '-':
-        return num0 - num1
-
-    elif operator == '*':
-        return num0 * num1
-    elif operator == '/':
-        return num0 / num1
-
     else:
         print('unknown operator!')
-        return None
+        return 0
